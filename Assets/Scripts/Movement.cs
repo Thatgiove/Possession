693a9d6b0a181d1379613f4a8f72d5455d7e54d3
Thatgiove <47024965+Thatgiove@users.noreply.github.com>
2022-11-02 15:33:01 +0100

    Movement -> fix gravity edit level

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,49 +6,47 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 6;
     [SerializeField] float jumpHeight = 2;
-    [SerializeField] float gravity = 90;
+    [SerializeField] float gravity = 50;
 
     Vector3 movementDirection;
     float airControl = 5;
     CharacterController characterController;
+    
     Creature c;
     MouseLook ml;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
-
         c = GetComponent<Creature>();
     }
 
     void FixedUpdate()
     {
+        if(!c) return;
+
         var input = new Vector3(Input.GetAxis("Horizontal"),  0, Input.GetAxis("Vertical"));
 
         input *= movementSpeed;
 
-        
-
         if (characterController.isGrounded)
         {
-            if (Input.GetButton("Jump"))
-            {
-
-            }
-            else
-            {
-
-            }
+            //if (Input.GetButton("Jump"))
+            //{
+            //}
+            //else
+            //{
+            //}
         }
         else
         {
-
-            if (c && c.isGravityEnabled)
+            if (c.isGravityEnabled)
             {
+                movementDirection.y -= gravity * Time.deltaTime;
                 input.y = movementDirection.y;
-                movementDirection = Vector3.Lerp(movementDirection, input, airControl * Time.deltaTime);
+                movementDirection = Vector3.Lerp(movementDirection, input, airControl * Time.deltaTime); 
             }
-            if (c && !c.isGravityEnabled)
+            else
             {
                 var ml = GetComponent<MouseLook>();
                 if (ml)
@@ -57,11 +55,6 @@ public class Movement : MonoBehaviour
                     movementDirection = transform.TransformDirection(ml.head.transform.localRotation * Vector3.forward );
                 }
             }
-        }
-
-        if (c && c.isGravityEnabled)
-        {
-            movementDirection.y -= gravity * Time.deltaTime;
         }
 
         input = transform.TransformDirection(input);
