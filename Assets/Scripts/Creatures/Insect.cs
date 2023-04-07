11e18e5b07fb188768de0be94e7cb70c3ed2177c
Thatgiove <47024965+Thatgiove@users.noreply.github.com>
2023-04-07 16:46:08 +0200

    Insect -> raycast pipe fix level

--- Assets/Scripts/Creatures/Insect.cs
+++ Assets/Scripts/Creatures/Insect.cs
@@ -1,30 +1,38 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Insect : MonoBehaviour
 {
     Rigidbody rb;
-    public Vector3 myNormal;
+    Vector3 myNormal;
     Camera cam;
-    float gravity = -2f;
+    float gravity = -6f;
     Transform head;
     float tiltAngle = 0;
     public float minX = -80f;
     public float maxX = 80.0f;
-    public Quaternion insectRotation;
+    Vector3 extraVect = Vector3.zero;
+    PlayerCanvas playerCanvas;
+    
     void Start()
     {
         head = transform.Find("head");
-        insectRotation = transform.localRotation;
         rb = GetComponent<Rigidbody>();
 
         myNormal = transform.up;
         if (rb)
             rb.freezeRotation = true;
+
+        playerCanvas = FindObjectOfType<PlayerCanvas>();
     }
 
 
     void Update()
     {
+        playerCanvas.creatureNameTxt.GetComponent<TMP_Text>().text = "";
+        playerCanvas.possessTxt.GetComponent<TMP_Text>().text = "";
+
         cam = GetComponentInChildren<Camera>();
         if (cam)
         {
@@ -32,25 +40,47 @@ public class Insect : MonoBehaviour
             Ray ray = cam.ScreenPointToRay(point);
             RaycastHit hit;
 
-            if (Input.GetButtonDown("Jump"))
+            /*TODO parametrizzare la distanza*/
+            if (Physics.Raycast(ray, out hit, 1))
             {
-                if (Physics.Raycast(ray, out hit, 3 /*TODO parametrizzare la distanza*/))
+                var wall = hit.transform;
+                
+                if (wall.tag == "Wall" || wall.tag == "Ceiling")
+                    playerCanvas.possessTxt.GetComponent<TMP_Text>().text = "[SPACE] Climb the wall";
+    
+          
+                if (Input.GetButtonDown("Jump"))
                 {
-                    var wall = hit.transform;
-
                     //TODO - trovare il calcolo per adesso il normale del soffitto è l'asse delle Y (up)
                     //mentre quello del muro e l'asse Z (forward)
                     if (wall.tag == "Ceiling")
                     {
+                        extraVect = Vector3.zero;
                         CalulateNewNormal(Vector3.Cross(wall.right, wall.forward));
                     }
                     else if (wall.tag == "Wall")
                     {
+                        extraVect = Vector3.zero;
                         CalulateNewNormal(Vector3.Cross(wall.right, wall.up));
                     }
                 }
             }
+
+            //Con questo Raycast vediamo su quale superficie stiamo camminando 
+            //TODO - non entrare sempre
+            if (Physics.Raycast(transform.position, -myNormal, out hit, 1))
+            {
+                //Quando entriamo nelle tubature la gravità ritorna normale
+                if(hit.transform.tag == "Pipe")
+                {
+                    extraVect = head.transform.forward;
+                    StartCoroutine(CalulateNewNormalAfterTime(Vector3.up));
+                }
+            }
+
+           
         }
+       
 
         var mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * 250;
 
@@ -81,14 +111,13 @@ public class Insect : MonoBehaviour
     {
         myNormal = wallNormal;
         var myForward = Vector3.Cross(transform.right, myNormal);
-        var dstRot = Quaternion.LookRotation(myForward, myNormal);
+        var dstRot = Quaternion.LookRotation(myForward + extraVect, myNormal);
         transform.rotation = dstRot;
-        insectRotation = dstRot;
     }
     void JumpToWall(Vector3 point, Vector3 normal)
     {
 
-        rb.isKinematic = true; // disable physics w$$anonymous$$le jumping
+        rb.isKinematic = true; 
         var orgPos = transform.position;
         var orgRot = transform.rotation;
         var dstPos = point + normal;
@@ -106,5 +135,12 @@ public class Insect : MonoBehaviour
 
         myNormal = normal; // update myNormal
         rb.isKinematic = false; // enable physics
+    }
+
+    IEnumerator CalulateNewNormalAfterTime(Vector3 newNormal)
+    {
+        yield return new WaitForSeconds(.5f);
+        CalulateNewNormal(newNormal);
+
     }
 }
